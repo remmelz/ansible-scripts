3c0fb04c73b420051bcb9ac6acf12a8e6e9447d1
Kees Remmelzwaal <kees@fastmail.com>
2018-03-14 18:24:22 +0100

    Minor changes.

--- scripts/play-role.sh
+++ scripts/play-role.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+homedir="/home/ansible"
 runyml="./tests/run.yml"
 
 if [[ -f ${runyml} ]]; then
   ansible-playbook $1 $2 $3 $4 ${runyml}
 
-elif [[ -d ~/$1 ]]; then
-  cd ~/$1
-  ansible-playbook $1 $2 $3 $4 ${runyml}
+elif [[ -d ${homedir}/$1 ]]; then
+  cd ${homedir}/$1
+  ansible-playbook ${runyml}
+
 fi
 
 exit 0
